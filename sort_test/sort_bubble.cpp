f397932ab574c17e0e9adfb7c672db58e42b94d5
Demar_Qiu <qlkeep_faith@163.com>
2021-05-16 13:03:40 +0800

    关于排序的算法学习

--- sort_test/sort_bubble.cpp
+++ sort_test/sort_bubble.cpp
@@ -98,7 +98,55 @@ void BubbleSort2(int array[], int size)
 }
 
 
+// 特殊 ：鸡尾酒排序算法（针对多数元素已经有序的情况，只有个别无序）
+void CocktailSort(int array[], int size)
+{
+	int temp = 0;
 
+	for (int i = 0; i < size/2; i++)
+	{
+
+		// 进行有序标记，初始值设为true  
+		bool isSorted = true;
+		// 奇数轮，从左向右比较并交换
+		for (int j = i; j < size-i-1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				//有元素交换的情况下，说明不是有序，标记变为false
+				isSorted = false;
+			}
+		}
+		if (isSorted)
+		{
+			break;
+		}
+
+		// 在偶数轮之前，将isSorted重新标记为true
+		isSorted = true;
+		// 偶数轮，从右向左比较并交换
+		for (int j = size - i - 1; j > i; j--)
+		{
+			if (array[j] < array[j - 1])
+			{
+				temp = array[j];
+				array[j] = array[j - 1];
+				array[j - 1] = temp;
+				// 因为有元素进行交换，所以不是有序
+				isSorted = false;
+			}
+		}
+		if (isSorted)
+		{
+			break;
+		}
+
+	}
+
+}
 
 
 
@@ -107,10 +155,12 @@ int main()
 	int array1[] = { 2,5,8,3,1,6,7,9,4 };
 	int array2[] = { 5,8,6,3,9,2,1,7 };
 	int array3[] = { 3,4,2,1,5,6,7,8 };
+	int array4[] = { 2,3,4,5,6,7,8,1 };
 
 	int len1 = sizeof(array1) / sizeof(int);
 	int len2 = sizeof(array2) / sizeof(int);
 	int len3 = sizeof(array3) / sizeof(int);
+	int len4 = sizeof(array4) / sizeof(int);
 
 	for (int k = 0; k < len1; k++)
 	{
@@ -155,6 +205,20 @@ int main()
 	}
 	cout << endl;
 
+	cout << "------------------------" << endl;
+
+	for (int k = 0; k < len4; k++)
+	{
+		cout << array4[k] << " ";
+	}
+	cout << endl;
+	CocktailSort(array4, len4);
+	cout << "The sorted array are:" << endl;
+	for (int k = 0; k < len4; k++)
+	{
+		cout << array4[k] << " ";
+	}
+	cout << endl;
 
 	return 0;
 }
